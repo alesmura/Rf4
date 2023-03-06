40f68fd26412f85cf19f9c21f830db917f0fed0f
aless <aless@LAPTOP-M6A4ME1H>
2023-03-07 00:19:34 +0100

    Scheduling batch

--- src/main/java/it/ghigo/Rf4Application.java
+++ src/main/java/it/ghigo/Rf4Application.java
@@ -1,9 +1,13 @@
 package it.ghigo;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableBatchProcessing
 public class Rf4Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Rf4Application.class, args);
--- src/main/java/it/ghigo/batch/Rf4Batch.java
+++ src/main/java/it/ghigo/batch/Rf4Batch.java
@@ -1,14 +1,18 @@
 package it.ghigo.batch;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import it.ghigo.model.Catch;
@@ -16,20 +20,36 @@ import it.ghigo.service.CatchService;
 
 @Configuration
 public class Rf4Batch {
+	@Autowired
+	private JobRepository jobRepository;
+	@Autowired
+	private PlatformTransactionManager transactionManager;
+	@Autowired
+	private JobLauncher jobLauncher;
+
+	@Scheduled(fixedDelay = 5000)
+	public void run() {
+		try {
+			JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
+					.toJobParameters();
+			jobLauncher.run(getRf4Job(), jobParameters);
+		} catch (Exception ex) {
+		}
+	}
 
 	@Bean
-	public Job getRf4Job(JobRepository jobRepository, JobExecutionListener listener, Step step) {
+	public Job getRf4Job() {
 		return new JobBuilder("rf4Job", jobRepository) //
 				.incrementer(new RunIdIncrementer()) //
-				.listener(listener) //
-				.flow(step) //
+				.listener(getListener()) //
+				.flow(getStep()) //
 				.end() //
 				.build();
 	}
 
 	@Bean
-	public Step getStep1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-		return new StepBuilder("step1", jobRepository) //
+	public Step getStep() {
+		return new StepBuilder("step", jobRepository) //
 				.<String, Catch>chunk(1000, transactionManager)//
 				.reader(getReader()) //
 				.processor(getProcessor()) //
