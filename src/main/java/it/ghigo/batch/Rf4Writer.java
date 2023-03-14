ec7df355a7b07267d1f9899573d7571a8adaea22
alesmura <alessio.murante@gmail.com>
2023-03-15 00:24:00 +0100

    Display writer

--- src/main/java/it/ghigo/batch/Rf4Writer.java
+++ src/main/java/it/ghigo/batch/Rf4Writer.java
@@ -2,6 +2,8 @@ package it.ghigo.batch;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,13 +12,18 @@ import it.ghigo.service.CatchService;
 
 public class Rf4Writer implements ItemWriter<Catch> {
 
+	private static final Logger log = LoggerFactory.getLogger(Rf4Writer.class);
+
 	@Autowired
 	private CatchService catchService;
 
 	@Override
 	public void write(List<? extends Catch> items) throws Exception {
+		long inizio = System.currentTimeMillis();
+		log.info("Start Rf4Writer " + items.size() + " record");
 		for (Catch cattura : items) {
 			catchService.create(cattura);
 		}
+		log.info("End Rf4Writer -> " + (System.currentTimeMillis() - inizio));
 	}
 }
